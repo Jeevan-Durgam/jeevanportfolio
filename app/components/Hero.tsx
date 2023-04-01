cdeee6f513c7fdc9021a37660ca1498e294614c8
Jeevan-Durgam <jeevan.durgam634@gmail.com>
2023-04-01 12:44:23 +0530

    navbar created

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,23 @@ import React from "react";
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <div>Hero</div>;
+  return (
+    <div className="w-3/4 mx-auto h-[70vh] bg-gradient-to-r from-slate-400 to-slate-600 rounded-lg mt-10 flex justify-around gap-6">
+      <img
+        src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=455&q=80"
+        alt="image"
+      />
+      <div className="flex items-center p-10 flex-col">
+        <h1>Hi!! I'm Jeevan</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+          delectus perspiciatis incidunt illo aliquid. Repudiandae earum rem
+          pariatur eaque eligendi ullam, possimus, esse commodi, at voluptates
+          molestiae dolorem nam placeat!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,8 +4,14 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div>
-      <h1 className="p-6">This is a navbar</h1>
+    <div className="flex justify-evenly py-6">
+      <div></div>
+      <div className="flex justify-between gap-6">
+        <a href="#hero">About Me</a>
+        <a href="#Skills">Skills</a>
+        <a href="#Works">Works</a>
+        <a href="#Footer">Contact Me</a>
+      </div>
     </div>
   );
 };
